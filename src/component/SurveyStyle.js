526ec5dd432ea1fc119c67f4f9838a4300a0f686
SeungWan Park <qkrtmddhks95@gmail.com>
2020-09-01 23:00:35 +0900

    Feat/3 프로젝트에 필요한 기본 컴포넌트를 구현한다.  (#4)

---
+++ src/component/SurveyStyle.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SurveyStyle = (props) => {
+  return <div></div>;
+};
+
+export default SurveyStyle;
---
+++ src/routes/Routes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../component/Home";
+import SurveyType from "../component/SurveyType";
+import SurveyStyle from "../component/SurveyStyle";
+import SurveyResult from "../component/SurveyResult";
+import MyPage from "../component/MyPage";
+
+const Routes = (props) => {
+  return (
+    <>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="survey-type">
+        <SurveyType />
+      </Route>
+      <Route path="survey-style">
+        <SurveyStyle />
+      </Route>
+      <Route path="survey-result">
+        <SurveyResult />
+      </Route>
+      <Route path="my-page">
+        <MyPage />
+      </Route>
+    </>
+  );
+};
+
+export default Routes;
