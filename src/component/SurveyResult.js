decc9f9d5232b1beb31dfbe9a38fa6cbb81d2e1e
toneyparky <qkrtmddhks95@gmail.com>
2020-09-03 00:26:46 +0900

    feat: SurveyResult 구현
    - url에 따라 동적으로 결과를 보여줌

--- src/component/SurveyResult.js
+++ src/component/SurveyResult.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { TYPE_RESULT, TYPES } from "../const/typeResult";
 
 const SurveyResult = (props) => {
-  return <div></div>;
+  const location = useLocation();
+
+  const typeResult = location.pathname.split("/")[2];
+  const history = useHistory();
+
+  const validateUrl = () => {
+    if (!TYPES.includes(typeResult) || !typeResult) {
+      history.push("/");
+    }
+  };
+
+  useEffect(() => validateUrl(), []);
+
+  return (
+    <>
+      {typeResult && TYPES.includes(typeResult) && (
+        <div>
+          <div>{TYPE_RESULT[typeResult].name}</div>
+          <div>{TYPE_RESULT[typeResult].description}</div>
+          <div>{TYPE_RESULT[typeResult].famousSaying}</div>
+          <div>{TYPE_RESULT[typeResult].lastSaying}</div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SurveyResult;
