8e505a0d7a77afdc4fe9c680dd36c4df9c47dc0c
toneyparky <qkrtmddhks95@gmail.com>
2020-09-02 22:21:51 +0900

    feat: SurveyType 구현
    - api 연결 미완료
    - 임시 css 적용

--- src/component/SurveyType.js
+++ src/component/SurveyType.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import SuppButton from "./common/SuppButton";
+import { STAGES } from "../const/stages";
 
 const SurveyType = (props) => {
-  return <div></div>;
+  const [stage, setStage] = useState(0);
+
+  const history = useHistory();
+
+  const updateStage = (nextStage) => {
+    if (nextStage > 9) {
+      // todo api to update survey-style
+      history.push("/survey-style");
+    } else {
+      setStage(nextStage);
+    }
+  };
+
+  return (
+    <div>
+      <div className="title">{STAGES[stage].question}</div>
+      <div className="survey-type-buttons">
+        <SuppButton
+          content={STAGES[stage].leftAnswer}
+          fontColor="#ffffff"
+          bgColor="#007bff"
+          handleClick={() => updateStage(STAGES[stage].leftDestination)}
+        />
+        <SuppButton
+          content={STAGES[stage].rightAnswer}
+          fontColor="#ffffff"
+          bgColor="#007bff"
+          handleClick={() => updateStage(STAGES[stage].rightDestination)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SurveyType;
