ceda1cdffdfa5d9398522500639be5311a3b0e8e
SeungWan Park <qkrtmddhks95@gmail.com>
2020-09-02 22:26:04 +0900

    Feat/7 유형 검사 페이지를 구현한다. (#8)
    * feat: survey-type stage 정보 추가
    
    * feat: SuppButton에 onClick 추가
    
    * feat: SurveyType 구현
    
    - api 연결 미완료
    - 임시 css 적용

--- src/component/common/SuppButton.js
+++ src/component/common/SuppButton.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SuppButton = ({ content, fontColor, bgColor, size, handleClick }) => {
-  return <div className="supp-button">{content}</div>;
+  return (
+    <div className="supp-button" onClick={handleClick}>
+      {content}
+    </div>
+  );
 };
 
 export default SuppButton;
